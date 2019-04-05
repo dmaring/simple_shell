7063f71c7a75769028e63f53ac3da4cfeeae177e
Thomas Graeff <676@holbertonschool.com>
2019-04-05 18:51:59 +0000

    add atoi and itoa converters

--- shell.h
+++ shell.h
@@ -10,11 +10,16 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+
+char *_itoa(int n, char *s);
 char *str_concat(char *s1, char *s2);
 char *_strdup(char *s);
 char *_strstr(char *haystack, char *needle);
 
+int _atoi(char *s);
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
+
+void reverse(char *s);
 
 #endif /*__SHELL_H__*/
