3f2a4b147e1dfd130f39104e66f58f436ea5185a
Thomas Graeff <676@holbertonschool.com>
2019-04-05 17:38:24 +0000

    add string helpers

---
+++ string_char.c
@@ -0,0 +1,103 @@
+#include "shell.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL on failure, or pointer to newly allocated space in memory
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int len1, len2, i, j = 0;
+	char *s;
+
+	if (s1 == NULL)
+		len1 = 0;
+	else
+	{
+		for (len1 = 0; s1[len1]; len1++)
+			;
+	}
+	if (s2 == NULL)
+		len2 = 0;
+	else
+	{
+		for (len2 = 0; s2[len2]; len2++)
+			;
+	}
+
+	j = len1 + len2 + 1;
+
+	s = malloc(sizeof(char) * j);
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < len1; i++)
+	{
+		s[i] = s1[i];
+	}
+	for (i = 0; i < len2; i++)
+	{
+		s[i + len1] = s2[i];
+	}
+	s[len1 + len2] = '\0';
+	return (s);
+}
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * @str: string to copy
+ * Return: pointer to allocated space
+ */
+
+char *_strdup(char *str)
+{
+	char *result;
+	int len;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (*(str + i))
+		i++;
+
+	len = i;
+
+	result = malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		*(result + i) = *(str + i);
+
+	return (result);
+}
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string to check
+ * @needle: substring to look for
+ * Return: pointer to beginning of located substring, or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		char *begin = haystack;
+		char *pattern = needle;
+
+		while (*haystack && *pattern && *haystack == *pattern)
+		{
+			haystack++;
+			needle++;
+		}
+		if (*pattern == '\0')
+			return (begin);
+
+		haystack = begin + 1;
+	}
+	return (NULL);
+}
