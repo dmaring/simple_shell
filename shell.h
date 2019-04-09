1481baba19252b774f21ae8cc031e2148c854099
Thomas Graeff <676@holbertonschool.com>
2019-04-09 17:24:47 +0000

    changed getenv prototype

--- shell.h
+++ shell.h
@@ -13,7 +13,7 @@
 
 extern char **environ;
 
-char *_getenv(char *s, char **environ);
+char *_getenv(char *name);
 char *_itoa(int n, char *s);
 char *read_line(void);
 char **split_line(char *line);
