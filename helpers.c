7f084a7fe4875713808509eda5022b9003cef0bf
Drew <andrew.maring@gmail.com>
2019-04-17 22:26:18 +0000

    Add iscmd helper function to helpers.c

--- helpers.c
+++ helpers.c
@@ -1,6 +1,23 @@
  #include "shell.h"
 
 /**
+ * iscmd - check if file is an executable
+ * @filename: filename as char pointer
+ *
+ * Return: 1 if file is executable, 0 if not
+ */
+int iscmd(char *filename)
+{
+	struct stat st;
+
+	stat(filename, &st);
+	if ((st.st_mode & S_IFREG) && (!access(filename, X_OK)))
+		return (1);
+	else
+		return (0);
+}
+
+/**
  * rm_nl - remove the last character newline from string
  * @lineptr: double pointer to string
  */
